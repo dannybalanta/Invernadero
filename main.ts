54e06f9c79505463256c9e942f996fceb0ef5256
dannybalanta <42857316+dannybalanta@users.noreply.github.com>
2020-11-03 18:57:31 -0500

    minimal changes

--- main.ts
+++ main.ts
@@ -43,33 +43,6 @@ input.onButtonPressed(Button.A, function () {
     // espera de 2 segundos hasta volver a empezar
     basic.pause(2000)
 })
-/**
- * Código de javascript
- */
-/**
- * Link del proyecto en Makecode:
- * 
- * https://makecode.microbit.org/_RMiAf7fRUXyF
- * 
- * Link del proyecto en Github:
- * 
- * https://github.com/dannybalanta/invernadero
- * 
- * Nota: es posible trabajar este código usando el condicional if o el bucle while, el resultado es exactamemte el mismo en ambos casos
- */
-/**
- * Invernadero - monitor de temperatura y humedad
- * 
- * Es una aplicación de microbit que gestiona un invernadero con dos variables: temperatura y humedad. Mantiene la temperatura (T) entre 8 a 30 grados celsius y la humedad (H) entre 200 a 600 unidades de humedad. Si la temperatura excede 30 grados y la humedad el valor de 600 produce unas alarmas de alerta en ambas variables. Lo que sucede se informa en el tablero LED mostrando T baja, T normal o OK y T alta en el panel LED, o si la humedad es normal (H. ok), alta (H.alta) o baja (H.baja). También se agrega un sonido de alarma en caso tal de que las temperaturas o humedades sean demasiado altas de acuerdo a las condiciones de trabajo.
- * 
- * En esencia este código de invernadero está basado en el de Salvemos a las tortugas
- * 
- * Curso Coding 4 Kids
- * 
- * Estudiante Danny Balanta
- * 
- * Fecha de presentación: 3 de Noviembre de 2020
- */
 input.onButtonPressed(Button.B, function () {
     // acciones al presionar el botón B
     // asociar la variable creada llamada humedad a la lectura del pin análogo P0
